c6fcd500d8c411f23b2de59d156e20c33a239a79
何仁锋 <herenfeng@yiwei.com>
2022-02-18 18:39:42 +0800

    搞起

---
+++ testcase/testcase1.py
@@ -0,0 +1,31 @@
+import unittest
+import send_requests
+import getyaml
+
+class Testrequest(unittest.TestCase):
+    host = ""
+    @classmethod
+    def setUpClass(cls):
+        path = "./../config/host.yaml"
+        data = getyaml.getyaml(path)
+        cls.host =data["企云宝"]["test"]
+
+    def setUp(self) -> None:
+        path = r"/media/add"
+        self.url = Testrequest.host+path
+        print(self.url)
+        self.modth = "post"
+        self.headers = {
+            "content - type": "application / json;charset = UTF - 8",
+            "cookie": "PHPSESSID=3bbv7bfg8sl951j7j9flho31q1;"
+        }
+        self.data = {
+
+        }
+    def test1(self):
+
+        print(Testrequest.host)
+
+
+if __name__ == '__main__':
+    unittest.main()
